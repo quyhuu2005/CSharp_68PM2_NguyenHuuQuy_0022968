9b4ac3d1ce124e99337d75c82324ed91cab29702
quyhuu2005 <quyhuu1572005@gmail.com>
2026-05-13 20:35:00 +0700

    Chuc nang dang nhap

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace QuanLySinhVien
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labelTitle.Location = new System.Drawing.Point(100, 20);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(300, 31);
+            this.labelTitle.TabIndex = 5;
+            this.labelTitle.Text = "Quản lý sinh viên";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(50, 100);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.TabIndex = 0;
+            this.labelEmail.Text = "Email:";
+
+            this.textBoxEmail.Location = new System.Drawing.Point(150, 97);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(250, 20);
+            this.textBoxEmail.TabIndex = 1;
+
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(50, 150);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(55, 13);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Mật khẩu:";
+            this.labelPassword.Click += new System.EventHandler(this.labelPassword_Click);
+
+            this.textBoxPassword.Location = new System.Drawing.Point(150, 147);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(250, 20);
+            this.textBoxPassword.TabIndex = 3;
+
+            this.buttonLogin.Location = new System.Drawing.Point(150, 200);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(100, 30);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Đăng nhập";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelEmail);
+            this.Name = "Form1";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySinhVien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string email = textBoxEmail.Text.Trim();
+            string password = textBoxPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập email và mật khẩu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (email == "quyhuu1572005@gmail.com" && password == "0022968")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+           
+                textBoxEmail.Clear();
+                textBoxPassword.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại! Email hoặc mật khẩu không đúng.", "Thất bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void labelPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
